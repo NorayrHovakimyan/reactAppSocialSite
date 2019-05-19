28546484f82b1710fea93d881220c97284d82172
Norayr <hovakimyan.norayr1@gmail.com>
2019-05-19 18:58:08 +0400

    skeleton of app

--- src/App.js
+++ src/App.js
@@ -8,8 +8,36 @@ function App() {
           <img src="images/header_logo.png" alt="header logo"/>
         </header>
         <nav className='nav'>
+            <ul>
+                <li><a href="/profile">Profile </a></li>
+                <li><a href="/content">Content </a></li>
+                <li><a href="/messages">Messages </a></li>
+                <li><a href="/settings">Settings </a></li>
+            </ul>
         </nav>
-        <div className='content'></div>
+        <div className='content'>
+           <div className='content_bg'>
+                <img src="images/content_bg.jpg" alt="content background"/>
+           </div>
+            <div>
+                avatar + description
+            </div>
+            <div>
+                My posts
+                <div>
+                    new post
+                </div>
+                <div>
+                    post 1
+                </div>
+                <div>
+                    post 2
+                </div>
+                <div>
+                    post 3
+                </div>
+            </div>
+        </div>
     </div>
   );
 }
